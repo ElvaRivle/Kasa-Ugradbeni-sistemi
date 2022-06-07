5e8d146da0e5a1f75c143c1aa074273c44bccb97
Elvir Vlahovljak <evlahovlja1@etf.unsa.ba>
2022-06-07 17:59:26 +0200

    radi sve

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'dart:async';
+import 'dart:typed_data';
+import 'dart:ffi';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+import 'package:mqtt_client/mqtt_server_client.dart' as mqtt;
+import 'package:mqtt_client/mqtt_client.dart' as mqttClient;
 
 void main() {
   runApp(const MaterialApp(
@@ -57,6 +61,92 @@ class NormalModeWidget extends StatefulWidget {
 
 class NormalModeState extends State<NormalModeWidget> {
   String _scanBarcode = '';
+  String successMessage = '';
+
+  int port                = 1883;
+  String username         = '';
+  String passwd           = '';
+
+  final mqtt.MqttServerClient client = mqtt.MqttServerClient("broker.hivemq.com", 'US_Projekat_Kasa_18702');
+
+  StreamSubscription? subscription;
+
+  //client.connect je async
+  //mada cemo je sinhrono pozivati!
+  void _connect() {
+    client.useWebSocket = false;
+    client.port = 1883;
+    client.logging(on: true);
+    client.keepAlivePeriod = 60;
+    client.onDisconnected = onDisconnected;
+    client.onConnected = onConnected;
+    client.secure = false;
+    client.autoReconnect = true;
+    client.setProtocolV311();
+    client.resubscribeOnAutoReconnect = true;
+
+    final connMess = mqttClient.MqttConnectMessage()
+        .withClientIdentifier('US_Projekat_Kasa_18702')
+        .startClean() // Non persistent session for testing
+        .withWillQos(mqttClient.MqttQos.atMostOnce);
+    client.connectionMessage = connMess;
+
+    try {
+      print("SPAJANJE!!!!");
+      client.connect();
+      print("USPJESNO SPAJANJE!!!!");
+    } catch (e) {
+      print(e);
+      client.disconnect();
+      print("PAO AWAIT CONNECT :( :( :( :(");
+    }
+    
+    //if (client.connectionStatus!.state != mqttClient.MqttConnectionState.connected) {
+    //  print("STATUS NIJE CONNECTED!!!!!!");
+    //  client.disconnect();
+    //}
+  }
+
+  /// The unsolicited disconnect callback
+  void onDisconnected() {
+    print(
+        'ONDISCONNECT!!!!');
+  }
+
+  /// The successful connect callback
+  void onConnected() {
+    print(
+        'ONCONNECT!!!!!');
+  }
+//TODO: OBRISATI PRINTOVI, STAVITI PORUKU USPJESNOG SLANJA
+  Future<void> SendBarCode() async {
+    try {
+      print("SALJEMO!!!!!!");
+      const pubTopic = 'US/Kasa/BarKod';
+      final builder = mqttClient.MqttClientPayloadBuilder();
+      builder.addString(_scanBarcode);
+
+      /// Subscribe to it
+      print('SUB!!!!!!!');
+      client.subscribe(pubTopic, mqttClient.MqttQos.atMostOnce);
+
+      /// Publish it
+      print('PUBLISH!!!!!!!');
+      client.publishMessage(
+          pubTopic, mqttClient.MqttQos.atMostOnce, builder.payload!);
+
+      setState(() {
+        successMessage = "Uspjesno slanje barkoda.";
+      });
+    }
+    on Exception {
+      setState(() {
+        successMessage = "Neuspjesno slanje barkoda.";
+      });
+    }
+  }
+
+
 
   @override
   void initState() {
@@ -66,7 +156,7 @@ class NormalModeState extends State<NormalModeWidget> {
   Future<void> startBarcodeScanStream() async {
     FlutterBarcodeScanner.getBarcodeStreamReceiver(
         '#ff6666', 'Otkazi', true, ScanMode.BARCODE)!
-        .listen((barcode) => print(barcode));
+        .listen((barcode) => {});
   }
 
   // Platform messages are asynchronous, so we initialize in an async method.
@@ -76,7 +166,6 @@ class NormalModeState extends State<NormalModeWidget> {
     try {
       barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
           '#ff6666', 'Otkazi', true, ScanMode.BARCODE);
-      print(barcodeScanRes);
     } on PlatformException {
       barcodeScanRes = 'Failed to get platform version.';
     }
@@ -88,6 +177,7 @@ class NormalModeState extends State<NormalModeWidget> {
 
     setState(() {
       _scanBarcode = barcodeScanRes;
+      successMessage = "";
     });
   }
 
@@ -106,13 +196,20 @@ class NormalModeState extends State<NormalModeWidget> {
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: <Widget>[
                           ElevatedButton(
-                              onPressed: () => scanBarcodeNormal(),
-                              child: Text('Skeniranje pojedinacnih artikala')),
+                              onPressed: () {
+                                _connect();
+                                scanBarcodeNormal();
+                              },
+                              child: const Text('Skeniranje pojedinacnih artikala')),
                           ElevatedButton(
                               onPressed: () => startBarcodeScanStream(),
-                              child: Text('Neprekidno skeniranje artikala')),
+                              child: const Text('Neprekidno skeniranje artikala')),
                           Text('Barkod: $_scanBarcode\n',
-                              style: TextStyle(fontSize: 20))
+                              style: const TextStyle(fontSize: 20)),
+                          ElevatedButton(
+                              onPressed: () => SendBarCode(),
+                              child: const Text("Posalji skenirani barkod")
+                          ),
                         ]));
               }))
         ),
